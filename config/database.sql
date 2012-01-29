f2cebbb814df639c29f0c42897c8cd632c332230
Nils Heinold <github@nilsheinold.de>
2012-01-29 07:53:01 +0100

    Code styling

--- config/database.sql
+++ config/database.sql
@@ -16,11 +16,11 @@
 
 CREATE TABLE `tl_module` (
   `nlsh_piwik_domain` varchar(255) NOT NULL default '',
-	`nlsh_piwik_id_site` int(10) unsigned NOT NULL default '0',
-	`nlsh_piwik_last_minutes` int(10) unsigned NOT NULL default '30',
+  `nlsh_piwik_id_site` int(10) unsigned NOT NULL default '0',
+  `nlsh_piwik_last_minutes` int(10) unsigned NOT NULL default '30',
   `nlsh_piwik_token_auth` varchar(255) NOT NULL default '',
-	`nlsh_piwik_range_start` varchar(10) NOT NULL default '',
+  `nlsh_piwik_range_start` varchar(10) NOT NULL default '',
   `nlsh_piwik_visits_start` int(10) unsigned NULL default NULL,
-	`nlsh_piwik_impressum` blob NULL,
-	`nlsh_piwik_noscan` char(1) NOT NULL default '0',
+  `nlsh_piwik_impressum` blob NULL,
+  `nlsh_piwik_noscan` char(1) NOT NULL default '0',
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
